adf5355a31a5fbd8a0bdfa8a41c2afdc97722f49
ayush212op <ayushpansare30@gmail.com>
2025-07-19 21:06:31 +0530

    Updated program

--- day_21_to_40.py
+++ day_21_to_40.py
@@ -583,3 +583,138 @@ else:
 print("\n🏆 Total amount won: ₹" + str(score))
 print("🙏 Thank you for playing KBC!\n")
 '''
+
+"""
+String formatting in python:
+
+String formatting can be done in python using the format method(the og).
+"""
+
+# str = "My name is {} and I lively in {}"
+# name = "Ayush"
+# city = "pune"
+# print(str.format(name, city))
+
+'''error'''
+# print(str.format(city, name))
+
+'''how to solve'''
+# str = "My name is {1} and I lively in {0}"
+# name = "Ayush"
+# city = "pune"
+# print(str.format(city, name))
+
+# txt = "For only {price:.2f} dollars!"
+# print(txt.format(price = 49))
+
+"""
+f-strings in python
+It is a new string formatting mechanism introduced by the PEP 498. 
+It is also known as Literal String Interpolation or more commonly 
+as F-strings (f character preceding the string literal). The primary 
+focus of this mechanism is to make the interpolation easier.
+
+When we prefix the string with the letter 'f', the string becomes the 
+f-string itself. The f-string can be formatted in much same as the 
+str.format() method. The f-string offers a convenient way to embed 
+Python expression inside string literals for formatting.
+"""
+
+# name = "Ayush"
+# city = "pune"
+# print(f"My name is {name} and I lively in {city}")
+
+'''shows actual string in the print statment'''
+# print(f"My name is {{name}} and I lively in {{city}}")
+
+# val = 'Geeks'
+# print(f"{val}for{val} is a portal for {val}.")
+
+'''We can use it in a single statement as well.'''
+# print(f"{2 * 30}")
+
+'''Precision of format specifier'''
+# price = float(input("Enter a float-point number:"))
+# print(f"This {{price:.2f}} limits the amount of data to be printed: {price:.2f}")
+
+# name = input("Enter a string:")
+# print(f"This {{name:.5s}} limits the amount of data to be printed: {name:.5s}")
+
+"""
+Docstrings in python:
+
+Python docstrings are the string literals that appear right after the definition 
+of a function, method, class, or module.
+"""
+
+# def square(n):
+#     '''Takes in a number n, returns the square of n'''
+#     print(n**2)
+# square(5)
+
+"""
+Python Comments vs Docstrings:
+
+Python Comments:
+Comments are descriptions that help programmers better understand the 
+intent and functionality of the program. They are completely ignored 
+by the Python interpreter.
+
+Python docstrings
+As mentioned above, Python docstrings are strings used right after 
+the definition of a function, method, class, or module. They are 
+used to document our code.
+
+We can access these docstrings using the doc attribute.
+"""
+
+"""
+Python doc attribute:
+Whenever string literals are present just after the definition of a function, 
+module, class or method, they are associated with the object as their doc 
+attribute. We can later use this attribute to retrieve this docstring.
+"""
+
+# def square(n):
+#     '''Takes in a number n, returns the square of n'''
+#     return n**2
+# print(square.__doc__)
+# print(square(2))
+
+"""
+PEP 8:
+PEP 8 is a document that provides guidelines and best practices on how to 
+write Python code. It was written in 2001 by Guido van Rossum, Barry Warsaw, 
+and Nick Coghlan. The primary focus of PEP 8 is to improve the readability 
+and consistency of Python code.
+
+PEP stands for Python Enhancement Proposal, and there are several of them. A PEP 
+is a document that describes new features proposed for Python and documents aspects 
+of Python, like design and style, for the community.
+"""
+
+"""
+The Zen of Python:
+    
+Long time Pythoneer Tim Peters succinctly channels the BDFL's guiding principles for 
+Python's design into 20 aphorisms, only 19 of which have been written down.
+
+Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+Flat is better than nested.
+Sparse is better than dense.
+Readability counts.
+Special cases aren't special enough to break the rules.
+Although practicality beats purity.
+Errors should never pass silently.
+Unless explicitly silenced.
+In the face of ambiguity, refuse the temptation to guess.
+There should be one-- and preferably only one --obvious way to do it.
+Although that way may not be obvious at first unless you're Dutch.
+Now is better than never.
+Although never is often better than *right* now.
+If the implementation is hard to explain, it's a bad idea.
+If the implementation is easy to explain, it may be a good idea.
+Namespaces are one honking great idea -- let's do more of those!
+"""
