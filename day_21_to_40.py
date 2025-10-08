f2377abba4f0b38937427732d6b9a7b9863f7326
ayush212op <ayushpansare30@gmail.com>
2025-10-08 21:55:49 +0530

    some change

--- day_21_to_40.py
+++ day_21_to_40.py
@@ -1179,15 +1179,21 @@ else:
 # print ("Out of loop")
 
 """Exception Handling
-Exception handling is the process of responding to unwanted or unexpected events when a computer program runs. Exception handling deals with these events to avoid the program or system crashing, and without this process, exceptions would disrupt the normal operation of a program.
+Exception handling is the process of responding to unwanted or unexpected events when a 
+computer program runs. Exception handling deals with these events to avoid the program or 
+system crashing, and without this process, exceptions would disrupt the normal operation of 
+a program.
 
 Exceptions in Python
-Python has many built-in exceptions that are raised when your program encounters an error (something in the program goes wrong).
+Python has many built-in exceptions that are raised when your program encounters 
+an error (something in the program goes wrong).
 
-When these exceptions occur, the Python interpreter stops the current process and passes it to the calling process until it is handled. If not handled, the program will crash.
+When these exceptions occur, the Python interpreter stops the current process and passes it 
+to the calling process until it is handled. If not handled, the program will crash.
 
 Python try...except
-try….. except blocks are used in python to handle errors and exceptions. The code in try block runs when there is no error. If the try block catches the error, then the except block is executed.
+try….. except blocks are used in python to handle errors and exceptions. The code in try block 
+runs when there is no error. If the try block catches the error, then the except block is executed.
 
 Syntax:
 try:
@@ -1195,3 +1201,47 @@ try:
      #exception
 except:
      #Soloution of generated exception"""
+     
+# a = input("Enter a number:")
+
+# try:
+#     print(f"multiplication table of the {int(a)} is give below :")
+#     for i in range(1,11):
+#         print(f"{int(a)} X {i} = {int(a)*i}")
+# except Exception as e:
+#     print(e)
+
+# try:
+#     num = int(input("Enter an integer: "))
+#     a = [10, 20, 30, 40, 50]
+# except ValueError:
+#     print("Number entered is not an integer.")
+# except IndexError:
+#     print("This index does not exist in the list.")
+"""
+Finally Clause
+The finally code block is also a part of exception handling. When we handle exception using the try 
+and except block, we can include a finally block at the end. The finally block is always executed, so 
+it is generally used for doing the concluding tasks like closing file resources or closing database 
+connection or may be ending the program execution with a delightful message.
+
+Syntax:
+try:
+   #statements which could generate 
+   #exception
+except:
+   #solution of generated exception
+finally:
+    #block of code which is going to 
+    #execute in any situation
+The finally block is executed irrespective of the outcome of try……except…..else blocks
+One of the important use cases of finally block is in a function which returns a value.
+"""
+try:
+    num = int(input("Enter an integer: "))
+except ValueError:
+    print("Number entered is not an integer.")
+else:
+    print("Integer Accepted.")
+finally:
+    print("This block is always executed.")
